39316ddd483548596c3411aebaf6c67a0a66bf81
Abisek Raut <abisek_raut@hotmail.com>
2025-05-13 23:58:46 -0700

    Save local changes to analyze_permits.py

--- analyze_permits.py
+++ analyze_permits.py
@@ -60,4 +60,3 @@ df.to_csv("march_2025_permit_insights.csv", index=False)
 print("\n✅ Data exported to 'march_2025_permit_insights.csv'")
 
 
-git remote add origin https://github.com/datasci-abi/building-permit-analysis.git
